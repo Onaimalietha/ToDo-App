0653e1a3ea4cefed6c9d28ffaa7da2f0cfd469e0
Onaimalietha <kouichi.but.the.ichi.is.silent@gmail.com>
2023-04-22 01:50:40 -0300

    Learning how to use queries to delete the checkboxes

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const newTodos = document.getElementById("newTodos");
-console.log("input value: ", input.value);
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     
-    console.log(input);
     const todoTxt = input.value;
+    
+    if(todoTxt){   
+        const newTodoListEl = document.createElement("li"); /* adiciona novo elemento da lista*/
+        newTodos.appendChild(newTodoListEl);
+        newTodoListEl.className = "todoList";
 
-    if(todoTxt){
-        const newTodoEl = document.createElement("li");
-        newTodoEl.innerHTML = todoTxt; /* adiciona novo elemento da lista*/
-        newTodos.appendChild(newTodoEl);
-
+        console.log("Todotxt :", todoTxt);
+        console.log("checkbox value : ", todoTxt);
+        
+        const newTodoCheck = document.createElement("input"); /* adiciona novo input tipo checkbox*/
+        newTodoCheck.type = "checkbox";
+        newTodoCheck.className = "checkbox-round";
+        newTodoCheck.id = "todoCheck";
+        
+        newTodoListEl.appendChild(newTodoCheck);
+        newTodoListEl.appendChild(document.createTextNode(todoTxt)); /* adds text to the list */
     }
+});
+
+let selectedCheckboxes = document.querySelectorAll(".todoList input[type=checkbox]");
+
+selectedCheckboxes.addEventListener("change", function(){
+        
+    console.log("New value is: " + this.checked);
+    selectedCheckboxes.forEach(function(item) {
+      item.parentElement.style.display = "none";
+    }, false);  
 });
