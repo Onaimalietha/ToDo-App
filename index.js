65ecc23c1032c15dbe70b64988178550db183048
Onaimalietha <kouichi.but.the.ichi.is.silent@gmail.com>
2023-04-23 02:30:53 -0300

    UI pronta, creio que preciso aprender sobre banco de dados para implementar o resto do projeto agora

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const newTodos = document.getElementById("newTodos");
 
+const dones = document.getElementById("dones");
+
+let selectedCheckboxes;
+let checkers
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     
@@ -11,26 +16,34 @@ form.addEventListener("submit", (e) => {
         const newTodoListEl = document.createElement("li"); /* adiciona novo elemento da lista*/
         newTodos.appendChild(newTodoListEl);
         newTodoListEl.className = "todoList";
-
-        console.log("Todotxt :", todoTxt);
-        console.log("checkbox value : ", todoTxt);
         
         const newTodoCheck = document.createElement("input"); /* adiciona novo input tipo checkbox*/
         newTodoCheck.type = "checkbox";
         newTodoCheck.className = "checkbox-round";
         newTodoCheck.id = "todoCheck";
-        
+
         newTodoListEl.appendChild(newTodoCheck);
         newTodoListEl.appendChild(document.createTextNode(todoTxt)); /* adds text to the list */
+    } 
+
+    selectedCheckboxes = document.querySelectorAll(".todoList input[type=checkbox]"); /* adds the new checkboxes to the query elements array */
+
+    checkers = document.getElementsByClassName("checkbox-round"); /* returns array of the elements with the class*/
+
+    for (var i = 0; i<checkers.length; i++){ /* cria um novo listener para cada evento da classe */
+        console.log("new listener");
+        checkers[i].addEventListener("change", checkersFunc);
     }
 });
 
-let selectedCheckboxes = document.querySelectorAll(".todoList input[type=checkbox]");
+function checkersFunc(){
+    selectedCheckboxes.forEach(deleteChecked); 
+}
 
-selectedCheckboxes.addEventListener("change", function(){
-        
-    console.log("New value is: " + this.checked);
-    selectedCheckboxes.forEach(function(item) {
-      item.parentElement.style.display = "none";
-    }, false);  
-});
+function deleteChecked(element){
+    if(element.checked){
+        element.disabled = true;
+        dones.appendChild(element.parentElement);
+    }
+}
+
